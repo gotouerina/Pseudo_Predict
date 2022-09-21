6491cfc823a4455fce3765a8723277d52d1d8cc0
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2022-09-21 11:43:47 +0800

    Add files via upload

---
+++ jiajiyin.py
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+def jiajiyin(input):
+    with open (input,"r") as a:
+        for line in a:
+            if line.startswith("Protein"):
+                chrom = line.strip().split()[3]
+                print ("%s" %chrom)
+            else:
+                continue
+
+if __name__=="__main__":
+    input = "genewise_cansu.out"
+    jiajiyin(input)
