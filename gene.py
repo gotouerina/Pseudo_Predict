af679451128f9145aca92c88a5bc5540c0d57848
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2022-10-04 09:57:39 +0800

    Add files via upload

---
+++ gene.py
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+def jiajiyin(input):
+    with open (input,"r") as a:
+        for line in a:
+            if line.startswith("Protein"):
+                chrom = line.strip().split()[1]
+                print ("%s" %chrom)
+            else:
+                continue
+
+if __name__=="__main__":
+    input = "Ecansus.pep.genewise.out"
+    jiajiyin(input)
---
+++ pro.py
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+def main(input):
+    with open(input,"r") as f:
+        for line in f:
+            if line.startswith("HSP"):
+                continue
+            if line.startswith(">"):
+                CHR = line.strip().split(">")[1]
+            else:
+                content = line.strip().split()
+                content[0] = CHR
+                print("%s %s %s" %(content[0],content[11],content[12]))
+                #print("\t".join(content))
+
+if __name__ == "__main__":
+    #input = "que.bed"
+    input = "Ecansus.pep.gba.report"
+    main(input)
